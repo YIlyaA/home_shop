bcd092705c214dad2fad3ee929f1c5b19bf21ce1
unknown <anukovich2@gmail.com>
2024-09-14 18:59:28 +0200

    filters with pagination solution conflicts

--- goods/views.py
+++ goods/views.py
@@ -4,20 +4,33 @@ from django.core.paginator import Paginator
 
 # Create your views here.
 def catalog(request, category_slug):
+
+    page = request.GET.get("page", 1)
+    on_sale = request.GET.get("on_sale", None)
+    order_by = request.GET.get("order_by", None)
+
+
     if category_slug == "all":
         goods = Products.objects.all()
     else:
         goods = get_object_or_404(Products.objects.filter(category__slug=category_slug))
+    
 
+    if on_sale:
+        goods = goods.filter(discount__gt=0)
+    if order_by == 'price' and order_by != 'default':
+        goods = goods.order_by(order_by)
 
-    page = request.GET.get("page", 1)
 
     paginator = Paginator(goods, 3)
     current_page = paginator.page(page)
 
+
+
     context = {
         "title": 'Home - Catalog',
         "goods": current_page,
+        "slug_url": category_slug,
     }
 
     return render(request, "goods/catalog.html", context)
