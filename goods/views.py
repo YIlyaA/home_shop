b8fdc12338cd593a27b9f4b9937a3bdea2f38138
unknown <anukovich2@gmail.com>
2024-09-14 13:23:55 +0200

    slug completed views

--- goods/views.py
+++ goods/views.py
@@ -1,9 +1,13 @@
-from django.shortcuts import render
-from goods.models import Categories, Products
+from django.shortcuts import get_list_or_404, get_object_or_404, render
+from goods.models import Products
 
 # Create your views here.
-def catalog(request):
-    goods = Products.objects.all()
+def catalog(request, category_slug):
+    if category_slug == "all":
+        goods = Products.objects.all()
+    else:
+        goods = get_object_or_404(Products.objects.filter(category__slug=category_slug))
+
 
     context = {
         "title": 'Home - Catalog',
