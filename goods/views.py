920720247400fd9a04bd65938c2cc112032db96d
unknown <anukovich2@gmail.com>
2024-09-14 13:03:14 +0200

    slug converter

--- goods/views.py
+++ goods/views.py
@@ -13,5 +13,11 @@ def catalog(request):
     return render(request, "goods/catalog.html", context)
 
 
-def product(request):
-    pass
+def product(request, product_slug):
+    product = Products.objects.get(slug=product_slug)
+
+    context = {
+        "product": product
+    }
+
+    return render(request, "goods/product.html", context)
