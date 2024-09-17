965950f5e9135073ad5b649e41d96252660a25b3
unknown <anukovich2@gmail.com>
2024-09-17 12:23:03 +0200

    cart model in progress

--- carts/models.py
+++ carts/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+from goods.models import Products
+from users.models import User
+
+
+class Cart(models.Model):
+
+    user = models.ForeignKey(to=User, on_delete=models.CASCADE, blank=True, null=True)
+    product = models.ForeignKey(to=Products, on_delete=models.CASCADE)
+    quantity = models.PositiveSmallIntegerField(default=0)
+    session_key = models.CharField(max_length=32, null=True, blank=True)
+    created_timestamp = models.DateTimeField(auto_now_add=True)
+
+    def products_price(self):
+        return round(self.product.display_price_with_discount() * self.quantity, 2)
+        
+
+
+    def __str__(self):
+        return f'Cart {self.user.username} | Product {self.product.name} | Quantity {self.quantity}'
