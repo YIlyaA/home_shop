d6361fc419019d7e4cacaa77a6e03a8482cfeaf5
unknown <anukovich2@gmail.com>
2024-09-18 13:51:56 +0200

    cart functional

--- users/views.py
+++ users/views.py
@@ -18,7 +18,9 @@ def login(request):
             if user:
                 auth.login(request, user)
                 messages.success(request, f"{username}, Now you are log in!")
-                if request.POST.get('next', None):
+
+                redirect_page = request.POST.get('next', None)
+                if redirect_page and redirect_page != reverse('user:logout'):
                     return HttpResponseRedirect(request.POST.get('next'))
                 return HttpResponseRedirect(reverse('main:index'))  # Функция reverse преобразует 'main:index' в url и перенаправляет 
     else:
